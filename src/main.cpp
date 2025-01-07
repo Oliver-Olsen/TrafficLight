c0c57550d6cd23daa847ab27d25bb06c57f7bc0a
Oliver Olsen <oliver@skou-olsen.dk>
2025-01-07 13:41:37 +0100

    Comments and Clean-up

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #include <Arduino.h>
+// Static variables that NEVER changes
 #define REDPIN 10
 #define YELLOWPIN 11
 #define GREENPIN 12
 
-// put function declarations here:
+// function declarations:
 void redToGreen();
 void greenToRed();
 
@@ -15,12 +16,15 @@ void setup() {
   pinMode(YELLOWPIN, OUTPUT); 
   pinMode(GREENPIN, OUTPUT); 
 
+
   //Turns the red light on, and the others off
   digitalWrite(REDPIN, HIGH);
   digitalWrite(YELLOWPIN, LOW);
   digitalWrite(GREENPIN, LOW);
   Serial.println("STOP!"); 
 }
+
+
 
 void loop() {
   delay(5000); //Light is red for five seconds 
@@ -29,22 +33,28 @@ void loop() {
   greenToRed(); //Turns the green light red
 }
 
-// put function definitions here:
+
+// Transistions the traffic light from red to green, by using various delays
 void redToGreen() {
+  // Red light is already set at the end of greenToRed()
     digitalWrite(YELLOWPIN, HIGH);
     Serial.println("Get Ready!"); 
     delay(2000);
+
     digitalWrite(REDPIN, LOW);
     digitalWrite(YELLOWPIN, LOW);
     digitalWrite(GREENPIN, HIGH);
     Serial.println("GO!"); 
 }
 
+// Transistions the traffic light from green to red, by using various delays
 void greenToRed(){
+    // Sets the green to low imidiately, since green has already been lit for a duration.
     digitalWrite(GREENPIN, LOW);
     digitalWrite(YELLOWPIN, HIGH);
     Serial.println("Slow Down!"); 
     delay(2000);
+
     digitalWrite(YELLOWPIN, LOW);
     digitalWrite(REDPIN, HIGH);
     Serial.println("STOP!"); 
