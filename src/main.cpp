1d3b2418a3b2f0fbb5282c456b56d248b437eeec
Metafysical <wulff43211234@gmail.com>
2025-01-07 14:06:57 +0100

    Added comments and answered questions

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,8 @@
+//2a: 
+//The modulus (%) operator outputs what is left over when the left side is cleanly divided by the number on the right. 
+//This means that 42 % 5 is 2, since 40 can be cleanly divided by 5 and 42 - 40 = 2
+//2b: 
+
 #include <Arduino.h>
 // Static variables that NEVER changes
 #define REDPIN 10
@@ -37,25 +42,27 @@ void loop() {
 // Transistions the traffic light from red to green, by using various delays
 void redToGreen() {
   // Red light is already set at the end of greenToRed()
-    digitalWrite(YELLOWPIN, HIGH);
-    Serial.println("Get Ready!"); 
-    delay(2000);
+    digitalWrite(YELLOWPIN, HIGH); //Turns on the yellow light
+    Serial.println("Get Ready!"); //Outputs text
+    delay(2000); //Waits for 2 seconds before turning the light green
 
-    digitalWrite(REDPIN, LOW);
-    digitalWrite(YELLOWPIN, LOW);
-    digitalWrite(GREENPIN, HIGH);
-    Serial.println("GO!"); 
+    digitalWrite(REDPIN, LOW); //Turns off the red light
+    digitalWrite(YELLOWPIN, LOW); //Turns off the yellow light
+    digitalWrite(GREENPIN, HIGH); //Turns on the green light
+    Serial.println("GO!"); //Outputs text
 }
 
 // Transistions the traffic light from green to red, by using various delays
 void greenToRed(){
     // Sets the green to low imidiately, since green has already been lit for a duration.
-    digitalWrite(GREENPIN, LOW);
-    digitalWrite(YELLOWPIN, HIGH);
-    Serial.println("Slow Down!"); 
-    delay(2000);
+    digitalWrite(GREENPIN, LOW); //Turns off the green light
+    digitalWrite(YELLOWPIN, HIGH); //Turns on the yellow light
+    Serial.println("Slow Down!"); //Outputs text
+    delay(2000); //Waits for 2 seconds before turning the light red
 
-    digitalWrite(YELLOWPIN, LOW);
-    digitalWrite(REDPIN, HIGH);
-    Serial.println("STOP!"); 
+    digitalWrite(YELLOWPIN, LOW); //Turns off the yellow light
+    digitalWrite(REDPIN, HIGH); //Turns on the red light
+    Serial.println("STOP!"); //Outputs text
 }
+
+//For 2c and 2d check binary counter code
