82bc4531628b1aac0d2e486338eac546849ec661
Metafysical <wulff43211234@gmail.com>
2025-01-07 12:46:04 +0100

    Loop and setup

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,24 @@ void redToGreen();
 void greenToRed();
 
 void setup() {
-  // put your setup code here, to run once:
-  redToGreen();
+  Serial.begin(9600); //Begins serial communication
+  
+  //Defines pinmodes
+  pinMode(REDPIN, OUTPUT); 
+  pinMode(YELLOWPIN, OUTPUT); 
+  pinMode(GREENPIN, OUTPUT); 
+
+  //Turns the red light on, and the others off
+  digitalWrite(REDPIN, HIGH);
+  digitalWrite(YELLOWPIN, LOW);
+  digitalWrite(GREENPIN, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(5000); //Light is red for five seconds 
+  redToGreen(); //Turns the red light green
+  delay(5000); //Light is green for five seconds
+  greenToRed(); //Turns the green light red
 }
 
 // put function definitions here:
